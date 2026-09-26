710bccf21e43b4607cd822909337b3e9b996749a
mythsunwind <scjost@protonmail.com>
2026-09-26 14:08:08 +0200

    Add button to swap artist and title

--- src/components/DownloadForm.ts
+++ src/components/DownloadForm.ts
@@ -104,31 +104,42 @@ export class DownloadForm extends HTMLElement {
 
   private render(): void {
     this.innerHTML = `
-    <form class="row g-3 needs-validation" novalidate>
-      <div class="row mb-3">
-        <div class="col-12 col-form-label">
+    <form class="row g-3 align-items-center needs-validation" novalidate>
+      <div class="row">
+        <div class="col-12 mb-3">
           Original video title: ${this.original}
           <input type="hidden" id="url" class="form-control" value="${this.url}" />
         </div>
       </div>
-      <div class="row mb-3">
-        <div class="col-2 col-form-label">
+      <div class="row">
+        <div class="col-2">
           <label for="artist" class="col-form-label">Artist</label>
         </div>
-        <div class="col-auto">
+        <div class="col-10">
           <input type="text" id="artist" class="form-control" value="${this.artist}" />
         </div>
       </div>
-      <div class="row mb-3">
-        <div class="col-2 col-form-label">
+      <div class="row">
+        <div class="col-2">
+        </div>
+        <div class="col-10 text-center">
+          <button id="swap" type="button" class="btn btn-light btn-sm">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-up" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5m-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5"/>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div class="row mb-5">
+        <div class="col-2">
           <label for="title" class="col-form-label">Title</label>
         </div>
-        <div class="col-auto">
+        <div class="col-10">
           <input type="text" id="title" class="form-control" value="${this.title}" />
         </div>
       </div>
       <div class="row mb-3">
-        <div class="col-12 col-form-label">
+        <div class="col-12">
           Original upload date: ${this.uploadDate}
         </div>
       </div>
@@ -174,6 +185,13 @@ export class DownloadForm extends HTMLElement {
 
       form.classList.add('was-validated')
     }, false)
+    const swapButton = document.getElementById("swap");
+    swapButton?.addEventListener("click", () => {
+      const artist = inputArtist.value;
+      const title = inputTitle.value;
+      inputArtist.value = title;
+      inputTitle.value = artist;
+    });
   }
 }
 
